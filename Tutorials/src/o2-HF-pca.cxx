392cabf576a609c3933a21039c448a9e953171cf
Koki Soeda <111231349+KoukiSoeda@users.noreply.github.com>
2022-12-01 17:57:22 +0100

    Add mother info

--- Tutorials/src/o2-HF-pca.cxx
+++ Tutorials/src/o2-HF-pca.cxx
@@ -20,17 +20,26 @@ struct pca_mc {
       "registry",
       {
          {"nParticle", "nParticle", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"muon_vx", ": x (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"muon_vy", ": y (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"muon_vz", ": z (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
          {"MuonPtEta", "; p_{T} (GeV/c); #eta; tracks", {HistType::kTH2F, {{600, 0 ,2*M_PI}, {35, -4.5, -1}}}},
-         {"MuonMotherParticle", "; pdgCode; counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}}
+         {"MuonMotherParticle", "; pdgCode; counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"MuonMotherParticle_vx", ": x (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"MuonMotherParticle_vy", ": y (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}},
+         {"MuonMotherParticle_vz", ": z (cm); counts", {HistType::kTH1F, {{nBins, -1000, 1000}}}}
       }
    };
 
    void process(MFTTracksLabeled const& mfttracks, aod::McParticles const& particles, aod::McTrackLabels const& labels){
       for(auto& mfttrack : mfttracks){
+         //Load MFT tracks information
          auto MFTTrackId = mfttrack.mcParticle();
          auto CollisionId = mfttrack.collisionId();
          auto PDGCode = MFTTrackId.pdgCode();
          registry.get<TH1>(HIST("nParticle"))->Fill(PDGCode);
+
+         //Get muon MC information
          if(PDGCode==13){
             auto mu_vx = MFTTrackId.vx();
             auto mu_vy = MFTTrackId.vy();
@@ -39,13 +48,23 @@ struct pca_mc {
             auto mu_eta = MFTTrackId.eta();
             auto mu_pt = MFTTrackId.pt();
             registry.get<TH2>(HIST("MuonPtEta"))->Fill(mu_pt, mu_eta);
+            registry.get<TH1>(HIST("muon_vx"))->Fill(mu_vx);
+            registry.get<TH1>(HIST("muon_vy"))->Fill(mu_vy);
+            registry.get<TH1>(HIST("muon_vz"))->Fill(mu_vz);
          }
          
+         //Get mother particle information
          if(MFTTrackId.has_mothers() && PDGCode==13){
             auto MotherParticles = MFTTrackId.mothersIds();
             auto motherId = particles.rawIteratorAt(MotherParticles[0]);
             auto MotherPDGCode = motherId.pdgCode();
+            auto mother_vx = motherId.vx();
+            auto mother_vy = motherId.vy();
+            auto mother_vz = motherId.vz();
             registry.get<TH1>(HIST("MuonMotherParticle"))->Fill(MotherPDGCode);
+            registry.get<TH1>(HIST("MuonMotherParticle_vx"))->Fill(mother_vx);
+            registry.get<TH1>(HIST("MuonMotherParticle_vy"))->Fill(mother_vy);
+            registry.get<TH1>(HIST("MuonMotherParticle_vz"))->Fill(mother_vz);
          }
 
       }
