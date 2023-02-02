f450484dcf0b9b7be648684a3f0a7ac6a4d3dbe7
Koki Soeda <111231349+KoukiSoeda@users.noreply.github.com>
2023-02-02 18:41:24 +0900

    Edit dca_pca_0130.cxx

--- Tutorials/src/dcs_pca_0130.cxx
+++ Tutorials/src/dcs_pca_0130.cxx
@@ -26,7 +26,6 @@
 using namespace std;
 using namespace o2; 
 using namespace o2::framework;
-using namespace o2::track;
 using namespace o2::soa;
 using SMatrix55 = ROOT::Math::SMatrix<double, 5, 5, ROOT::Math::MatRepSym<double, 5>>;
 using SMatrix5 = ROOT::Math::SVector<Double_t, 5>;
@@ -36,11 +35,23 @@ using MFTTracksLabeled = soa::Join<o2::aod::MFTTracks, aod::McMFTTrackLabels>;
 struct DCAandPCA {
    Service<TDatabasePDG> pdg;
    Configurable<bool> useEvSel{"useEvSel", true, "use event selection"};
+   Configurable<float> zMax{"zMax", 5., "value for Zvtx cut"};
    
    HistogramRegistry registry{
       "registry",
       {
-         {"MFTTrack_z", " ; z(cm); ", {HistType::kTH1F, {{20000, -100, 100}}}},
+         {"muTrack_dcax", " ; x(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"muTrack_dcay", " ; y(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"muTrack_mftx", " ; x(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"muTrack_mfty", " ; y(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"D0decayx", " ; x(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"D0decayy", " ; y(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"D0decayz", " ; z(cm); ", {HistType::kTH1F, {{5000, -50, 50}}}},
+         {"pcax", " ; x(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"pcay", " ; y(cm); ", {HistType::kTH1F, {{10000, -100, 100}}}},
+         {"pcaz", " ; z(cm); ", {HistType::kTH1F, {{5000, -50, 50}}}},
+         {"PIDpurity", "", {HistType::kTH1F, {{102, -0.5, 100.5}}}},
+         {"EstimatePDGcode", "", {HistType::kTH1F, {{2002, -1000.5, 1000.5}}}},
       }
    };
 
@@ -53,19 +64,111 @@ struct DCAandPCA {
          if((collision.mcCollision().posZ() < zMax) && (collision.mcCollision().posZ() > -zMax)){
             if(!useEvSel || (useEvSel && collision.sel8())){
                float dcaXY;
-               o2::track::TrackParCovFwd trackPar;
-               for(auto& track : tracks){
-                  if(!track.has_mcParticle()) continue;
-                  auto particle = track.mcParticle();
-                  auto t_collision = track.collision();
-                  
-                  if(fabs(particle.pdgCode())!=13) continue;
-                  if(particle.mcCollisionId() == collision.mcCollision().globalIndex()){
-                     if(particle.has_mothers()){
-                        auto mcMom = particleMC.rawIteratorAt(particle.mothersIds()[0]);
-                        //if(fabs(mcMom.pdgCode()==421){
+               for(auto& track0 : tracks){
+                  if(!track0.has_mcParticle()) continue;
+                  auto particle0 = track0.mcParticle();
+                  auto t_collision = track0.collision();
+                  if(fabs(particle0.pdgCode())!=13) continue;
+                  int estpdg;
+                  int64_t truthK_ID,muonID,estID;
+                  float vx_mu,vy_mu,vz_mu,px_mu,py_mu,pz_mu,t_mu,mft_mu_x,mft_mu_y,s_mu,dca_mu_x,dca_mu_y;
+                  float vx_can,vy_can,vz_can,px_can,py_can,pz_can,t_can,mft_can_x,mft_can_y,s_can,dca_can_x,dca_can_y;
+                  float s,t,a,b,c,d,e,f,g,h;
+                  float mu_x,mu_y,mu_z,can_x,can_y,can_z,pca_x,pca_y,pca_z;
+                  if(particle0.mcCollisionId() == collision.mcCollision().globalIndex()){
+                     if(particle0.has_mothers()){
+                        int signMUON = 0;
+                        float closest_track = 10000;
+                        auto mcMom = particleMC.rawIteratorAt(particle0.mothersIds()[0]);
+                        auto Daughters = mcMom.daughters_as<aod::McParticles>();
+                        registry.fill(HIST("D0decayx"), particle0.vx());
+                        registry.fill(HIST("D0decayy"), particle0.vy());
+                        registry.fill(HIST("D0decayz"), particle0.vz());
 
-                        //}
+                        //Actual particles information
+                        if(fabs(mcMom.pdgCode())==421){
+                           signMUON++;
+                           for(auto Daughter : Daughters){
+                              if(fabs(Daughter.pdgCode())==321){
+                                 truthK_ID = Daughter.globalIndex();
+                              }
+                              if(fabs(Daughter.pdgCode())==13){
+                                 vx_mu = particle0.vx();
+                                 vy_mu = particle0.vy();
+                                 vz_mu = particle0.vz();
+                                 px_mu = particle0.px();
+                                 py_mu = particle0.py();
+                                 pz_mu = particle0.pz();
+                                 t_mu = (-46.0-vz_mu)/pz_mu;
+                                 mft_mu_x = px_mu*t_mu;
+                                 mft_mu_y = py_mu*t_mu;
+                                 s_mu = vz_mu/(vz_mu+46.0);
+                                 dca_mu_x = vx_mu+s_mu*(mft_mu_x-vx_mu);
+                                 dca_mu_y = vy_mu+s_mu*(mft_mu_y-vy_mu);
+                                 muonID = particle0.globalIndex();
+                                 a = dca_mu_x;
+                                 b = mft_mu_x;
+                                 c = dca_mu_y;
+                                 d = mft_mu_y;
+                                 //cout << "DCA_x: " << a << "DCA_y: " << c << endl;
+                                 registry.fill(HIST("muTrack_dcax"), a);
+                                 registry.fill(HIST("muTrack_dcay"), c);
+                                 registry.fill(HIST("muTrack_mftx"), b);
+                                 registry.fill(HIST("muTrack_mfty"), d);
+                              }
+                           }
+                        }
+                        if(signMUON==0) continue;
+                        for(auto& track1 : tracks){
+                           if(!track1.has_mcParticle()) continue;
+                           auto particle1 = track1.mcParticle();
+                           if(particle1.globalIndex()==muonID) continue;
+                           vx_can = particle1.vx();
+                           vy_can = particle1.vy();
+                           vz_can = particle1.vz();
+                           px_can = particle1.px();
+                           py_can = particle1.py();
+                           pz_can = particle1.pz();
+                           t_can = (-46.0-vz_can)/pz_can;
+                           mft_can_x = px_can*t_can;
+                           mft_can_y = py_can*t_can;
+                           s_can = vz_can/(vz_can+46.0);
+                           dca_can_x = vx_can+s_can*(mft_can_x-vx_can);
+                           dca_can_y = vy_can+s_can*(mft_can_y-vy_can);
+                           e = dca_can_x;
+                           f = mft_can_x;
+                           g = dca_can_y;
+                           h = mft_can_y;
+
+                           float i = pow(b,2)+pow(d,2)+pow(46,2);
+                           float j = pow(f,2)+pow(h,2)+pow(46,2);
+                           float k = b*f+d*h+46*46;
+
+                           s = (k*(-a*f+e*f-c*h+g*h)+j*(a*b-b*e+c*d-d*g))/(pow(k,2)-i*j);
+                           t = (k*s+a*f-e*f+c*h+g*h)/j;
+
+                           mu_x = dca_mu_x+s*mft_mu_x;
+                           mu_y = dca_mu_y+s*mft_mu_y;
+                           mu_z = -46*s;
+                           can_x = dca_can_x+t*mft_can_x;
+                           can_y = dca_can_y+t*mft_can_y;
+                           can_z = -46*t;
+
+                           float distance_track = pow(mu_x-can_x,2)+pow(mu_y-can_y,2)+pow(mu_z-can_z,2);
+                           if(distance_track<=closest_track){
+                              closest_track = distance_track;
+                              pca_x = (mu_x+can_x)/2;
+                              pca_y = (mu_y+can_y)/2;
+                              pca_z = (mu_z+can_z)/2;
+                              estpdg = particle1.pdgCode();
+                              estID = particle1.globalIndex();
+                           }
+                        }
+                        registry.fill(HIST("pcax"), pca_x);
+                        registry.fill(HIST("pcay"), pca_y);
+                        registry.fill(HIST("pcaz"), pca_z);
+                        registry.fill(HIST("PIDpurity"), fabs(estID-truthK_ID));
+                        registry.fill(HIST("EstimatePDGcode"), estpdg);
                      }
                   }
                }
